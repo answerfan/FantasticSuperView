477a2da2e771455bcf89ee357ffc048804624c3f
Aleksandr Boriskin <boriskin@improveitgroup.com>
2019-10-01 00:13:08 +0700

    update visibility

--- FantasticSuperView/FantasticSuperView.swift
+++ FantasticSuperView/FantasticSuperView.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FantasticSuperView: UIView {
+public class FantasticSuperView: UIView {
 
     let colors : [UIColor] = [.red, .orange, .yellow, .green, .blue, .purple]
     var colorCounter = 0
